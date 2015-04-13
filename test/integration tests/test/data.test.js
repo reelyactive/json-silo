b2a8fa3b87e3a301b2f7d87e36b50bc89c9983a0
gcool-info <george.koulouris1@gmail.com>
2015-04-13 13:08:27 -0400

    Data get route

--- test/integration tests/test/data.test.js
+++ test/integration tests/test/data.test.js
@@ -1,4 +1,4 @@
-describe("SAVE DATA", function() {
+describe("SAVE & GET DATA", function() {
 
   before(function(done) {
 
@@ -39,5 +39,31 @@ describe("SAVE DATA", function() {
         expect(savedData["schema:owns"]["schema:productID"]).to.equal(testData.jsonLD["schema:owns"][0]["schema:productID"]);
         done();
       });
-  })
+  });
+
+  it("should get the unencrypted data", function(done) {
+
+    // Login a user
+    request(url)
+      .get('data')
+      .set('Authorization', 'Bearer ' + testUser.token)
+      .send()
+
+      .end(function(err, res) {
+        if (err) {
+          throw err;
+        }
+
+        var savedData = res.body.data;
+        expect(savedData["@type"]).to.equal(testData.jsonLD["@type"]);
+        expect(savedData["@id"]).to.equal(testData.jsonLD["@id"]);
+        expect(savedData["schema:name"]).to.equal(testData.jsonLD["schema:name"]);
+        expect(savedData["schema:image"]).to.equal(testData.jsonLD["schema:image"]);
+        expect(savedData["schema:owns"]["@id"]).to.equal(testData.jsonLD["schema:owns"][0]["@id"]);
+        expect(savedData["schema:owns"]["@type"]).to.equal(testData.jsonLD["schema:owns"][0]["@type"]);
+        expect(savedData["schema:owns"]["schema:productID"]).to.equal(testData.jsonLD["schema:owns"][0]["schema:productID"]);
+        done();
+      });
+  });
+  
 });
