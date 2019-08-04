8a7641066add6b38de8fa7da10f605910355a169
jeffyactive <jeff@reelyactive.com>
2019-08-04 19:47:31 -0400

    Added cormorant to simplify story retrieval

--- web/json-silo/stories/js/stories.js
+++ web/json-silo/stories/js/stories.js
@@ -4,40 +4,13 @@
  */
 
 
-// Constants
-const STATUS_OK = 200;
-
-
 // DOM elements
 let visualStory = document.querySelector('#visualStory');
 let machineStory = document.querySelector('#machineStory');
 
 
-// Initialisation: GET the story and display in DOM
-getStory(window.location.href, function(status, response) {
-  machineStory.textContent = JSON.stringify(response, null, 2);
-
-  if(status === STATUS_OK) {
-    let storyId = Object.keys(response.stories)[0];
-    let story = response.stories[storyId];
-
-    cuttlefish.render(story, visualStory);
-  }
-  else { /* TODO: handle bad requests */ }
+// Retrieve and render the story
+cormorant.retrieveStory(window.location.href, function(story) {
+  machineStory.textContent = JSON.stringify(story, null, 2);
+  cuttlefish.render(story, visualStory);
 });
-
-
-// GET the story
-function getStory(url, callback) {
-  let httpRequest = new XMLHttpRequest();
-
-  httpRequest.onreadystatechange = function() {
-    if(httpRequest.readyState === XMLHttpRequest.DONE) {
-      return callback(httpRequest.status,
-                      JSON.parse(httpRequest.responseText));
-    }
-  };
-  httpRequest.open('GET', url);
-  httpRequest.setRequestHeader('Accept', 'application/json');
-  httpRequest.send();
-}
