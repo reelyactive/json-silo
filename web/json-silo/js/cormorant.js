84a7a839bcf508ede1d203c1c0ee295e1bc25a5d
jeffyactive <jeff@reelyactive.com>
2024-08-07 16:03:29 -0400

    Updated to latest web style guide

--- web/json-silo/js/cormorant.js
+++ web/json-silo/js/cormorant.js
@@ -1,5 +1,5 @@
 /**
- * Copyright reelyActive 2016-2020
+ * Copyright reelyActive 2016-2023
  * We believe in an open Internet of Things
  */
 
@@ -11,8 +11,9 @@ let cormorant = (function() {
   const SIGNATURE_SEPARATOR = '/';
 
   // Internal variables
-  let associations = {};
-  let stories = {};
+  let associations = new Map();
+  let stories = new Map();
+  let digitalTwins = new Map();
 
   // Extract the JSON-LD, if present, from the given HTML
   function extractFromHtml(html) {
@@ -22,72 +23,71 @@ let cormorant = (function() {
     }
     let startIndex = html.indexOf('>', tagIndex) + 1;
     let stopIndex = html.indexOf('</script>', startIndex);
-    let jsonString = html.substring(startIndex, stopIndex);
-    
-    return parseAsStory(jsonString);
-  }
-
-  // Parse the given stringified JSON as a standardised story
-  function parseAsStory(jsonString) {
-    let json = null;
 
     try {
-      json = JSON.parse(jsonString);
-
-      // Handle standard reelyActive API response case
-      if(json.hasOwnProperty('stories')) {
-        let storyId = Object.keys(json.stories)[0];
-        let story = json.stories[storyId];
-        return story;
-      }
+      return parseAsStory(JSON.parse(html.substring(startIndex, stopIndex)));
     }
-    catch(e) { }
+    catch(error) {
+      return null;
+    }
+  }
 
-    return json;
+  // Parse the given JSON as a standardised story
+  function parseAsStory(data) {
+    // Handle standard reelyActive API response case
+    if(data.hasOwnProperty('stories')) {
+      let storyId = Object.keys(data.stories)[0];
+      let story = data.stories[storyId];
+
+      return story;
+    }
+
+    return data;
   }
 
   // Perform a HTTP GET on the given URL with the given accept headers
   function retrieve(url, acceptHeaders, callback) {
-    let httpRequest = new XMLHttpRequest();
-
-    httpRequest.onreadystatechange = function() {
-      if(httpRequest.readyState === XMLHttpRequest.DONE) {
-        let contentType = httpRequest.getResponseHeader('Content-Type');
-        return callback(httpRequest.status, httpRequest.responseText,
-                        contentType);
-      }
-    };
-    httpRequest.open('GET', url);
-    httpRequest.setRequestHeader('Accept', acceptHeaders);
-    httpRequest.send();
+    fetch(url, { headers: { "Accept": acceptHeaders } })
+      .then((response) => {
+        if(!response.ok) { throw new Error('GET returned ' + response.status); }
+        let contentType = response.headers.get('Content-Type');
+        if(contentType.startsWith('application/json')) {
+          return response.json();
+        }
+        return response.text();
+      })
+      .then((result) => { return callback(result); })
+      .catch((error) => { return callback(null); });
   }
 
-  // Get the associations for the given device identifier
-  function retrieveAssociations(serverUrl, deviceId, isStoryToBeRetrieved,
-                                callback) {
-    let url = serverUrl + '/associations/' + deviceId;
-    retrieve(url, 'application/json', function(status, responseText) {
+  // Get the associations for the given device signature
+  function retrieveAssociations(serverUrl, deviceSignature, options, callback) {
+    options = options || {};
+    let url = serverUrl + '/associations/' + deviceSignature;
+
+    retrieve(url, 'application/json', (data) => {
       let deviceAssociations = null;
       let isStoryBeingRetrieved = false;
+      let isJsonData = (data !== null) && (typeof data === 'object');
 
-      if(status === STATUS_OK) {
-        let response = JSON.parse(responseText);
+      if(isJsonData) {
         let returnedDeviceId = null;
-        if(response.hasOwnProperty('associations')) { // chickadee v1.x
-          returnedDeviceId = Object.keys(response.associations)[0];
-          deviceAssociations = response.associations[returnedDeviceId];
+        if(data.hasOwnProperty('associations')) { // chickadee v1.x
+          returnedDeviceSignature = Object.keys(data.associations)[0];
+          deviceAssociations = data.associations[returnedDeviceSignature];
         }
-        else if(response.hasOwnProperty('devices')) { // chickadee v0.x
-          returnedDeviceId = Object.keys(response.devices)[0];
-          deviceAssociations = response.devices[returnedDeviceId];
+        else if(data.hasOwnProperty('devices')) { // chickadee v0.x
+          returnedDeviceSignature = Object.keys(data.devices)[0];
+          deviceAssociations = data.devices[returnedDeviceSignature];
         }
-        associations[deviceId] = deviceAssociations;
-        associations[returnedDeviceId] = deviceAssociations;
+        associations.set(deviceSignature, deviceAssociations);
+        associations.set(returnedDeviceSignature, deviceAssociations);
 
-        if(isStoryToBeRetrieved && deviceAssociations.url) {
+        if(options.isStoryToBeRetrieved && deviceAssociations.url) {
           isStoryBeingRetrieved = true;
-          retrieveStory(deviceAssociations.url, function(story) {
-            return callback(deviceAssociations, story);
+          retrieveStory(deviceAssociations.url, options,
+                        (story, isRetrievedFromMemory) => {
+            return callback(deviceAssociations, story, isRetrievedFromMemory);
           });
         }
       }
@@ -99,38 +99,116 @@ let cormorant = (function() {
   }
 
   // Get the story for the given URL
-  function retrieveStory(storyUrl, callback) {
-    if(stories.hasOwnProperty(storyUrl)) {
-      return callback(stories[storyUrl]);
+  function retrieveStory(storyUrl, options, callback) {
+    options = options || {};
+
+    if(stories.has(storyUrl) && !options.isStoryToBeRefetched) {
+      return callback(stories.get(storyUrl), true);
     }
 
-    retrieve(storyUrl, 'application/json, text/plain',
-             function(status, responseText, contentType) {
-      if(status !== STATUS_OK) {
-        return callback(null);
-      }
+    retrieve(storyUrl, 'application/json, text/plain', (data) => {
+      if(!data) { return callback(null, false); }
 
-      let isJson = (contentType.indexOf('application/json') === 0);
-      let story;
-      if(isJson) {
-        story = parseAsStory(responseText);
-      }
-      else {
-        story = extractFromHtml(responseText);
-      }
-      if(story) {
-        stories[storyUrl] = story;
-      }
-      return callback(story);
+      let isJsonData = (typeof data === 'object');
+      story = isJsonData ? parseAsStory(data) : extractFromHtml(data);
+      if(story) { stories.set(storyUrl, story); }
+
+      return callback(story, false);
     });
+  }
+
+  // Get the digital twin for the given device
+  function retrieveDigitalTwin(deviceSignature, device, options, callback) {
+    options = options || {};
+
+    if(digitalTwins.has(deviceSignature)) {
+      // TODO: update digital twin timestamp, refresh if necessary?
+      return callback(digitalTwins.get(deviceSignature), true);
+    }
+
+    if(!associations.has(deviceSignature) && options.associationsServerUrl) {
+      retrieveAssociations(options.associationsServerUrl, deviceSignature,
+                           { isStoryToBeRetrieved: true },
+                           (deviceAssociations, story) => {
+        updateDigitalTwin(deviceSignature, story);
+        return callback(digitalTwins.get(deviceSignature), false);
+      });
+    }
+    else if(device) {
+      if(device.url) {
+        retrieveStory(device.url, {}, (story) => {
+          updateDigitalTwin(deviceSignature, story);
+          // TODO: device.statid.uri
+          return callback(digitalTwins.get(deviceSignature), false);
+        });
+      }
+      else if(device.statid && device.statid.uri) {
+        retrieveStory(device.statid.uri, {}, (story) => {
+          updateDigitalTwin(deviceSignature, story);
+          return callback(digitalTwins.get(deviceSignature), false);
+        });
+      }
+    }
+    else {
+      return callback(null);
+    }
+  }
+
+  // Update the digital twin of the given device using the given story
+  function updateDigitalTwin(deviceSignature, story) {
+    if(!story) { return; }
+
+    let storyCovers = [];
+
+    if(story.hasOwnProperty('@graph') && Array.isArray(story['@graph'])) {
+      story['@graph'].forEach(storyElement => {
+        let storyCover = determineStoryCover(storyElement);
+        if(storyCover) { storyCovers.push(storyCover); }
+      });
+      digitalTwins.set(deviceSignature, { story: story,
+                                          storyCovers: storyCovers });
+    }
+    else {
+      // TODO: transform to graph (flattened) representation
+    }
+  }
+
+  // Determine the title and image for the storyCover
+  function determineStoryCover(element) {
+    let title;
+    let imageUrl;
+
+    if(element.hasOwnProperty("schema:name")) {
+      title = element["schema:name"];
+    }
+    else if(element.hasOwnProperty("schema:givenName") ||
+            element.hasOwnProperty("schema:familyName")) {
+      title = (element["schema:givenName"] || '') + ' ' +
+             (element["schema:familyName"] || '');
+    }
+
+    if(element.hasOwnProperty("schema:image")) {
+      imageUrl = element["schema:image"];
+    }
+    else if(element.hasOwnProperty("schema:logo")) {
+      imageUrl = element["schema:logo"];
+    }
+
+    if(title || imageUrl) {
+      return { title: title || '', imageUrl: imageUrl }
+    }
+
+    return null;
   }
 
   // Expose the following functions and variables
   return {
     retrieveAssociations: retrieveAssociations,
     retrieveStory: retrieveStory,
+    retrieveDigitalTwin, retrieveDigitalTwin,
     associations: associations,
-    stories: stories
+    stories: stories,
+    digitalTwins: digitalTwins
   }
 
 }());
